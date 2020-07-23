12cf4f2f2e20a7263d08c07a50fe386e06d25491
Little Code Master <advaitjoshi15@gmail.com>
2020-07-23 14:23:55 -0400

    if the numbers can be written a**n where n is given

---
+++ divisor_of_power_of_n.py
@@ -0,0 +1,33 @@
+from math import sqrt
+print("Give me two numbers 'a' and 'b' and I will tell you all the numbers up to 'a' that are not divisible by any number power 'b'. First, give me 'a'.")
+a=int(input())
+print("Now, give me 'b'.")
+b=int(input())
+print("The such numbers are written below.")
+c=0
+prime_list=[2]
+while c**b<=a:
+    c=c+1
+for d in range(3, c):
+    e=0
+    f=0
+    while prime_list[f]<sqrt(d):
+        if d%prime_list[f]==0:
+            e=1
+            break
+        f=f+1
+    if e==0:
+        prime_list.append(d)
+for c in range(2, a):
+    d=0
+    e=0
+    while (prime_list[e]**b)<=c:
+        if c%(prime_list[e]**b)==0:
+            d=1
+            break
+        if not (e+1)==len(prime_list):
+            e=e+1
+        else:
+            break
+    if d==0:
+        print(c)
