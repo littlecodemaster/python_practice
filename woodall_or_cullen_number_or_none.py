f924a52dbfb1a5ac008ce5170d9e2567773bb80a
Little Code Master <advaitjoshi15@gmail.com>
2020-07-28 14:06:06 -0400

    n*2^n(+-)1 or not

--- woodall_or_cullen_number_or_none.py
+++ woodall_or_cullen_number_or_none.py
@@ -3,9 +3,11 @@ a=int(input())
 b=0
 c=0
 while (b*(2**b))-2<a:
-    #import pdb
-    #pdb.set_trace()
-    if ((b*(2**b))+1)==a:
+    if a==1:
+        print("1 is a cullen number when n is 0 and a woodall number when n is 1.")
+        c=1
+        break
+    elif ((b*(2**b))+1)==a:
         print(a, "is a cullen number when n is", b, end="")
         print(".")
         c=1
